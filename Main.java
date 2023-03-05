4cbd5bf9cd7b86f1de533dd93a7bba246668647f
Apollo <jackratermann@gmail.com>
2023-03-05 02:19:50 -0800

    Small Edits

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 public class Main{
     public static void main(String[] args) {
         //Main jp = new Main();
-        System.out.println(first("uhifjknlvdfijouiYfjkD"));
+        System.out.println(capitalizeWord("hi me"));
     }
     static char first(String str){
         if(str.length() == 0){
@@ -12,5 +12,22 @@ public class Main{
             return str.charAt(0);
         } else
         return first(str.substring(1, str.length()));
+    }
+
+    static String capitalizeWord(String s){
+        if(s.length() == 0){
+            return s;
+        }
+
+        char chr = s.charAt(s.length()-1);
+
+        if(s.length() == 1){
+            return Character.toString(Character.toUpperCase(chr));
+        }
+        
+        if(s.substring(s.length()-2, s.length()-1).equals(" ")){
+            return Character.toString(Character.toUpperCase(chr)); 
+        } else
+        return capitalizeWord(s.substring(1, s.length()-1));
     }
 }
