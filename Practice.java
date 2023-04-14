1553e534daa83cebd2a6c5a8e3b9f97d17582be7
Apollo <jackratermann@gmail.com>
2023-04-14 14:10:51 -0700

    Nothing Happned

--- Practice.java
+++ Practice.java
@@ -1,15 +1,5 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        String yourMom = "this is your favorite food.";
-        System.out.println(capitalizeWord(yourMom));
-    }
-    public static String capitalizeWord(String str){
-      
-        String yourDad = str.substring(0,1).toUpperCase();
         
-
-       
-        return yourDad;
-      }
+    }
 }
