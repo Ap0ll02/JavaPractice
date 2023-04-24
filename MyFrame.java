e780259535a13d737b45304455232ea3d48a3f2c
Apollo <jackratermann@gmail.com>
2023-04-23 21:27:03 -0700

    Created my own mini GUI Game

--- MyFrame.java
+++ MyFrame.java
@@ -1,14 +1,37 @@
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame implements ActionListener {
+    JButton button;
     MyFrame(){
-        this.setTitle("This is my epic frame");
+        ImageIcon icon = new ImageIcon("medusa.png");
+        button = new JButton();
+        button.setBounds(200,100,500,500);
+        button.addActionListener(e -> System.out.println("Epicness!"));
+        button.setText("Press Me Please!");
+        button.setFocusable(false);
+        button.setIcon(icon);
+        button.setHorizontalTextPosition(JButton.CENTER);
+        button.setVerticalTextPosition(JButton.BOTTOM);
+        button.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        button.setForeground(new Color(250, 0, 0, 0));
+        button.setIconTextGap(50);
+        this.setTitle("Microsoft Word");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(1280, 720);
-        this.setResizable(true);
+        this.setSize(750, 750);
+        this.setLayout(null);
         this.setVisible(true);
-        this.getContentPane().setBackground(new Color(80,0,255));
+        
+        this.add(button);
     }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
 }
--- Practice.java
+++ Practice.java
@@ -1,31 +1,72 @@
 import javax.swing.*;
-import javax.swing.border.Border;
-
 import java.awt.*;
 public class Practice {
+    public static int buttonStat = 0;
     public static void main(String[] args) {
-        //JFrame Practice
-        MyFrame myFrame = new MyFrame(); //Frame Tutorial Found in MyFrame.java
-        
-       
-        //JLabel Tests
-        MyLabel label = new MyLabel(); //Create a label
-        myFrame.add(label);
 
-        //JPanel For Sectioning Parts of a Frame
-        JPanel panel1 = new JPanel();
-        JPanel panel2 = new JPanel(null, false);
-        JPanel panel3 = new JPanel(null, false);
-        panel1.setBackground(new Color(255, 120, 100));
-        panel2.setBackground(Color.green);
-        panel3.setBackground(new Color(0,0,120));
-        panel1.setBounds(50, 0, 50, 110);
-        panel2.setBounds(520, 0, 250, 250);
-        panel3.setBounds(0, 800, 400, 33);
-        panel1.add(label);
-        panel1.setLayout(new BorderLayout(5, 5));
-        myFrame.add(panel1);
-        myFrame.add(panel2);
-        myFrame.add(panel3);
+        //TODO: Figure out what is going on what's going on with the button scaling!
+
+        //Create JFrame and call method to setup basics
+        JFrame frame = new JFrame("The Window");
+        Practice p = new Practice();
+        p.createFrame(frame);
+        frame.setVisible(true);
+        
+        //Create JLabel title and call method to fill in info
+        JLabel title = new JLabel("Welcome to our adventure!");
+        p.createLabel(title); //Method sets title and other chracteristics.
+        frame.add(title); 
+
+        //Create and run button creation method.
+        JButton button = new JButton();
+        p.createButton(button);
+        frame.add(button);
+
+        //Button Listener Changes The scene of everything via Lambda function ->. 
+        button.addActionListener(e -> p.SceneRunner(button, title, frame));
+    }
+
+    //FRAME STUFF
+    void createFrame(JFrame frame){
+        frame.setResizable(false);
+        frame.setSize(1280, 720);
+        frame.setBackground(new Color(50, 80, 50));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    //LABEL STUFF
+    void createLabel(JLabel title){
+        title.setHorizontalTextPosition(JLabel.CENTER);
+        title.setVerticalTextPosition(JLabel.TOP);
+        //title.setHorizontalAlignment(JLabel.CENTER);
+        //title.setVerticalAlignment(JLabel.TOP);
+        title.setBounds(340, 80, 850, 100);
+        title.setFont(new Font("Phosphate", Font.ITALIC, 48));
+    }
+    
+    //BUTTON STUFF
+    void createButton(JButton button){
+        button.setText("Begin Adventure");
+        button.setFont(new Font("Phosphate", Font.BOLD, 36));
+        button.setForeground(new Color(0, 50, 200));
+        button.setBounds(440, 450, 450, 150);
+        button.setFocusable(true);
+    }
+
+    //Attempting to remake the scene.
+    void SceneRunner(JButton button, JLabel label, JFrame frame){
+        if(buttonStat == 0){
+            label.setText("Quien es tu persona favorita");
+            button.setText("Micaela");
+            button.setBounds(440, 450, 450, 150);
+            button.setFocusable(true);
+        }
+        else if(buttonStat == 1){
+            label.setText("You made it farther!");
+            button.setText("Great!");
+            button.setBounds(440, 450, 450, 150);
+            button.setFocusable(true);
+        }
+        buttonStat++;
     }
 }
